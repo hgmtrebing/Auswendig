f223876408e48b8ab6eccce8b13c0c0a4ab1d77a
Harry Geoffrey Trebing <hgmtrebing@gmail.com>
2024-08-17 13:09:26 -0400

    Add SpringDoc OpenAPI integration and User Controller
    Included SpringDoc OpenAPI dependency in the pom.xml to enable API documentation. Added `UserController` with a basic getUser endpoint for retrieving user information. Created `SwaggerConfiguration` to setup OpenAPI details for the application.

---
+++ src/main/java/us/hgmtrebing/auswendigserver/rest/SwaggerConfiguration.java
@@ -0,0 +1,25 @@
+package us.hgmtrebing.auswendigserver.rest;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI auswendigSwaggerApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Auswendig API")
+                        .version("1.0")
+                        .description("RESTful API for Auswendig Flash Card Application.")
+                        .contact(new Contact()
+                                .email("hgmtrebing@outlook.com")
+                                .name("Harry Trebing")
+                        )
+                );
+    }
+}
