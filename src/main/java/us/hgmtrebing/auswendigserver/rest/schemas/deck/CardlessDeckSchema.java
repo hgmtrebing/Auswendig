861540794108589dc794cc6d66d483f95c71d766
Harry Geoffrey Trebing <hgmtrebing@gmail.com>
2024-09-01 13:00:41 -0400

    Add global hints and notes for deck sides
    This update introduces global hint and note fields for all cards and sides in the CardlessDeckSchema class. Additionally, it adds specific template fields for Side 01 and Side 02, including their names, descriptions, datatypes, and individual global hints and notes.

--- src/main/java/us/hgmtrebing/auswendigserver/rest/schemas/deck/CardlessDeckSchema.java
+++ src/main/java/us/hgmtrebing/auswendigserver/rest/schemas/deck/CardlessDeckSchema.java
@@ -26,4 +26,47 @@ public class CardlessDeckSchema {
 
     @Schema(description = "The username of the deck's owner.", example = "orabbit")
     private String ownerUsername;
+
+    @Schema(description = "A global hint for all cards and sides.")
+    private String globalHint;
+
+    @Schema(description = "A global note for all cards and sides.")
+    private String globalNote;
+
+
+    // Side 01 Template Fields
+
+    @Schema(description = "The name of Side 01 for the Deck.")
+    private String side01Name;
+
+    @Schema(description = "The description of Side 01 for the Deck.")
+    private String side01Description;
+
+    @Schema(description = "The datatype of Side 01 for the Deck.")
+    private SideTypeSchema side01Type;
+
+    @Schema(description = "A global hint for Side 01.")
+    private String side01GlobalHint;
+
+    @Schema(description = "A global note for Side 01.")
+    private String side01GlobalNote;
+
+
+    // Side 02 Template Fields
+
+    @Schema(description = "The name of Side 02 for the Deck.")
+    private String side02Name;
+
+    @Schema(description = "The description of Side 02 for the Deck.")
+    private String side02Description;
+
+    @Schema(description = "The datatype of Side 02 for the Deck.")
+    private SideTypeSchema side02Type;
+
+    @Schema(description = "A global hint for Side 02.")
+    private String side02GlobalHint;
+
+    @Schema(description = "A global note for Side 02.")
+    private String side02GlobalNote;
+
 }
